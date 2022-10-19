f1c7f8f69de186be246326ab4449c7aa9e533df4
edgarbp95 <edgarbp95@hotmail.com>
2022-10-19 11:13:17 -0300

    actualización de algunos estilos

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -25,7 +25,7 @@ const Footer = () => {
             <p>El porcentaje de la tasa de interés siempre será el mismo durante todo el plazo del préstamo. Esto permite que el monto de la letra no cambie. Nuestra tasa efectiva anual va desde el 165% al 177%, según perfil del cliente. Nuestros plazos desde los 3 y hasta los 18 meses, además le financiaremos los gastos de formalización, los cuales van incluidos en el préstamo otorgado.</p>
             <p><span>EJEMPLO DE PRESTAMO:</span> Monto solicitrado por el cliente: $100.00, Monto principal incluyendo los gastos de formalización: $277,00 letra mensual por 12 meses de $23,08.</p>
         </div>
-        <div>
+        <div className='footer-floating-card'>
             <FloatingCard />
         </div>
       
